2dcab49c3875ef48b46f70413189bb7c378aca7b
Marcelo Oikawa <moikawa@localhost.localdomain>
2016-04-25 22:20:05 -0300

    Adding "hello world" and module structure

---
+++ code/src/main/java/com/bigspin/Main.java
@@ -0,0 +1,11 @@
+package com.bigspin;
+
+/**
+ * Created by moikawa on 4/25/16.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
